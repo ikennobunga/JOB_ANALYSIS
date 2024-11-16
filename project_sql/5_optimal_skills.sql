8d393db4311d53ceb4718c7c6ad7953b78895f28
kenn <ikennobunga@users.noreply.github.com>
2024-11-16 09:29:31 +0300

    update on optimal_skills file refactoring the query

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -1,61 +1,92 @@
-WITH skills_demand AS (
-  SELECT
-    skills_dim.skill_id,
-    skills_dim.skills,
-    job_postings_fact.job_title_short AS job_title,
-    COUNT(skills_job_dim.job_id) AS demand_count
-  FROM
-    job_postings_fact
-    INNER JOIN
-      skills_job_dim
-      ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN
-      skills_dim
-      ON skills_job_dim.skill_id = skills_dim.skill_id
-  WHERE
-    job_title_short = 'Data Analyst'
-    AND salary_year_avg IS NOT NULL
-    AND job_work_from_home = TRUE
-  GROUP BY
-    skills_dim.skill_id,
-    job_postings_fact.job_title_short
-),
-avarage_salary AS (
-  SELECT
-    skills_dim.skill_id,
-    skills_dim.skills,
-    ROUND(
+-- WITH skills_demand AS (
+--   SELECT
+--     skills_dim.skill_id,
+--     skills_dim.skills,
+--     job_postings_fact.job_title_short AS job_title,
+--     COUNT(skills_job_dim.job_id) AS demand_count
+--   FROM
+--     job_postings_fact
+--     INNER JOIN
+--       skills_job_dim
+--       ON job_postings_fact.job_id = skills_job_dim.job_id
+--     INNER JOIN
+--       skills_dim
+--       ON skills_job_dim.skill_id = skills_dim.skill_id
+--   WHERE
+--     job_title_short = 'Data Analyst'
+--     AND salary_year_avg IS NOT NULL
+--     AND job_work_from_home = TRUE
+--   GROUP BY
+--     skills_dim.skill_id,
+--     job_postings_fact.job_title_short
+-- ),
+-- avarage_salary AS (
+--   SELECT
+--     skills_dim.skill_id,
+--     skills_dim.skills,
+--     ROUND(
+--       AVG(job_postings_fact.salary_year_avg),
+--       0
+--       ) AS avg_yearly_salary
+--   FROM
+--     job_postings_fact
+--     INNER JOIN
+--       skills_job_dim
+--       ON job_postings_fact.job_id = skills_job_dim.job_id
+--     INNER JOIN
+--       skills_dim
+--       ON skills_job_dim.skill_id = skills_dim.skill_id
+-- WHERE
+--   job_title_short = 'Data Analyst'
+--   AND salary_year_avg IS NOT NULL
+--   AND job_work_from_home = TRUE
+-- GROUP BY
+--   skills_dim.skill_id
+-- )
+
+-- SELECT
+--   skills_demand.skill_id,
+--   skills_demand.skills,
+--   demand_count,
+--   avg_yearly_salary
+-- FROM
+--   skills_demand
+--   INNER JOIN
+--     avarage_salary
+--     ON skills_demand.skill_id = avarage_salary.skill_id
+-- WHERE
+--   demand_count > 10
+-- ORDER BY
+--   avg_yearly_salary DESC
+-- LIMIT 25
+
+
+-- rewriting this same query more concisely
+
+SELECT
+  skills_dim.skill_id,
+  skills_dim.skills,
+  COUNT(skills_job_dim.job_id) AS demand_count,
+  ROUND(
       AVG(job_postings_fact.salary_year_avg),
       0
-      ) AS avg_yearly_salary
-  FROM
-    job_postings_fact
-    INNER JOIN
-      skills_job_dim
-      ON job_postings_fact.job_id = skills_job_dim.job_id
-    INNER JOIN
-      skills_dim
-      ON skills_job_dim.skill_id = skills_dim.skill_id
+    ) AS avg_yearly_salary
+FROM
+  job_postings_fact
+  INNER JOIN
+    skills_job_dim
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+  INNER JOIN
+    skills_dim
+    ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
   job_title_short = 'Data Analyst'
   AND salary_year_avg IS NOT NULL
   AND job_work_from_home = TRUE
 GROUP BY
   skills_dim.skill_id
-)
-
-SELECT
-  skills_demand.skill_id,
-  skills_demand.skills,
-  demand_count,
-  avg_yearly_salary
-FROM
-  skills_demand
-  INNER JOIN
-    avarage_salary
-    ON skills_demand.skill_id = avarage_salary.skill_id
-WHERE
-  demand_count > 10
+HAVING
+  COUNT(skills_job_dim.job_id) > 10
 ORDER BY
-  avarage_salary DESC
+  avg_yearly_salary DESC
 LIMIT 25
